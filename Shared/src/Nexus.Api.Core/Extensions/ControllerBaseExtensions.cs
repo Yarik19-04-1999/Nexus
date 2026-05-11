d1ce42258e6e40886400f9b30e4e0cd4dc21d1f0
Yaroslav Balaba <ybalaba@teamworkcommerce.com>
2026-05-11 08:57:14 +0300

    Add domain response mapper

---
+++ Shared/src/Nexus.Api.Core/Extensions/ControllerBaseExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nexus.Api.Core.Mappers;
+using Nexus.Application.Core.Models;
+
+namespace Nexus.Api.Core.Extensions;
+
+public static class ControllerBaseExtensions
+{
+    public static ObjectResult DomainError(this ControllerBase controller, Result result)
+        => new(result.MapToDomainErrorResponse()) 
+        { 
+            StatusCode = StatusCodes.Status418ImATeapot 
+        };
+}
---
+++ Shared/src/Nexus.Api.Core/Mappers/ResponsesMappers.cs
@@ -0,0 +1,11 @@
+﻿using Nexus.Api.Core.ViewModels;
+using Nexus.Application.Core.Models;
+using Riok.Mapperly.Abstractions;
+
+namespace Nexus.Api.Core.Mappers;
+
+[Mapper(RequiredMappingStrategy = RequiredMappingStrategy.Target)]
+public static partial class ResponsesMappers
+{
+    public static partial DomainErrorResponse MapToDomainErrorResponse(this Result domainError);
+}

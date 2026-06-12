f526924b8d7fff9af3fee83f6fb8471aa9f9b76a
Yaroslav Balaba <ybalaba@teamworkcommerce.com>
2026-06-12 21:52:21 +0300

    Add default services add middlewares

--- Dvizh/src/Dvizh.Api/Program.cs
+++ Dvizh/src/Dvizh.Api/Program.cs
@@ -1,16 +1,33 @@
 using Dvizh.Application.Extensions;
 using Nexus.Api.Core.Extensions;
+using Nexus.Infrastructure.Core.Constants;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddNexusApiVersioning();
-builder.Services.AddApplication(builder.Configuration, builder.Environment);
+builder.Services.AddNexusOpenApi();
+builder.Services.AddNexusCorrelationId();
+builder.Services.AddNexusResponseCompression();
+builder.Services.AddNexusRequestTimeouts();
 builder.Services.AddNexusCors(builder.Configuration);
+builder.Services.AddNexusHealthChecks()
+    .AddNexusSqlServerHealthCheck(builder.Configuration[$"{OptionsConstants.SqlServer.SectionName}:ConnectionString"]!);
+builder.Services.AddApplication(builder.Configuration, builder.Environment);
 
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+app.UseNexusSecurityHeaders();
+app.UseNexusCorrelationId();
+app.UseNexusExceptionHandling();
+app.UseNexusResponseCompression();
+app.UseNexusRequestTimeouts();
 app.UseCors();
 app.UseStaticFiles();
+
 app.MapControllers();
+app.MapNexusHealthChecks();
+app.MapNexusScalarUi();
+
 app.Run();
--- Information/src/Information.Api/Program.cs
+++ Information/src/Information.Api/Program.cs
@@ -6,10 +6,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddNexusApiVersioning();
+builder.Services.AddNexusOpenApi();
+builder.Services.AddNexusCorrelationId();
+builder.Services.AddNexusResponseCompression();
+builder.Services.AddNexusRequestTimeouts();
+builder.Services.AddNexusHealthChecks();
 builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+app.UseNexusSecurityHeaders();
+app.UseNexusCorrelationId();
+app.UseNexusExceptionHandling();
+app.UseNexusResponseCompression();
+app.UseNexusRequestTimeouts();
+
 app.MapControllers();
+app.MapNexusHealthChecks();
+app.MapNexusScalarUi();
+
 app.Run();
